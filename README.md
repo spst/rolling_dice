2c739a3b24e4bd48333b1509be3b7b68308fde87
Stefan <spst@users.noreply.github.com>
2020-03-09 12:54:47 -0400

    include basic functionality from CS50w/src6/dice*

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rolling_dice
-WebApp to simulate rolling of dice
+# Rolling Dice
+WebApp to simulate rolling dice
---
+++ index.js
@@ -0,0 +1,20 @@
+const template = Handlebars.compile(document.querySelector('#result').innerHTML);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#roll').onclick = ()  => {
+
+        // Generate random rolls.
+        const counter = parseInt(document.querySelector('#counter').value);
+        const rolls = [];
+        let total = 0;
+        for (let i = 0; i < counter; i++) {
+            const value = Math.floor(Math.random() * 6) +  1;
+            rolls.push(value);
+            total += value;
+        };
+
+        // Add roll results to DOM.
+        const content = template({'values': rolls, 'total': total});
+        document.querySelector('#rolls').innerHTML = content;
+    };
+});
