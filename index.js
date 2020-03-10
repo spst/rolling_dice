8383e652aec04a6a61d6f6c16ef4e587864af0ee
Stefan <spst@users.noreply.github.com>
2020-03-10 09:09:06 -0400

    refactor index.js

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const template = Handlebars.compile(document.querySelector('#result').innerHTML);
 
+
+/** return a random integer in the interval [1, max_val] */
+function roll(max_val=6) {
+    return Math.floor(Math.random() * max_val) +  1;
+};
+
+
+/** return a lis of random integers in the interval [1, max_val] */
+function roll_multiple_dice(num_dice=1, max_val=6) {
+    const rolls = [];
+    for (let i = 0; i < num_dice; i++) {
+        const value = roll(max_val);
+        rolls.push(value);
+    };
+    return rolls;
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#roll').onclick = ()  => {
+    /** onclick action for Roll button */
+    document.getElementById("button_roll").onclick = ()  => {
         // Generate random rolls.
-        const counter = parseInt(document.querySelector('#select_dicecount').value);
-        const rolls = [];
-        let total = 0;
-        for (let i = 0; i < counter; i++) {
-            const value = Math.floor(Math.random() * 6) +  1;
-            rolls.push(value);
-            total += value;
-        };
+        const num_dice = parseInt(document.getElementById('select_dicecount').value);
+        const dice_type = parseInt(document.getElementById("select_dicetype").value);
+
+        const rolls = roll_multiple_dice(num_dice, dice_type);
 
         // Add roll results to DOM.
-        const content = template({'values': rolls, 'total': total});
-        document.querySelector('#rolls').innerHTML = content;
+        const content = template({'values': rolls});
+        document.getElementById('rolls').innerHTML = content;
     };
 });
