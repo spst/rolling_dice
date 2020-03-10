706e452d3d041485ff142f34771291b7077aebc4
Stefan <spst@users.noreply.github.com>
2020-03-10 09:39:42 -0400

    use white dice, refactor dice display

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const template = Handlebars.compile(document.querySelector('#result').innerHTML);
+const template_dice_6 = Handlebars.compile(document.getElementById('dice_6').innerHTML);
 
 
 /** return a random integer in the interval [1, max_val] */
@@ -18,6 +18,24 @@ function roll_multiple_dice(num_dice=1, max_val=6) {
 };
 
 
+function clear_dice_in_dom() {
+    document.getElementById('dice_display').innerHTML = null;
+};
+
+
+function add_die_to_dom(template, die_value, image_size) {
+    const content = template({"value": die_value, "height": image_size, "width": image_size});
+    document.getElementById('dice_display').innerHTML += content;
+};
+
+
+function add_dice_to_dom(template, dice_values, image_size) {
+    for (let i = 0; i < dice_values.length; i++) {
+        add_die_to_dom(template, dice_values[i], image_size);
+    };
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
     /** onclick action for Roll button */
     document.getElementById("button_roll").onclick = ()  => {
@@ -28,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const rolls = roll_multiple_dice(num_dice, dice_type);
 
         // Add roll results to DOM.
-        const content = template({'values': rolls});
-        document.getElementById('rolls').innerHTML = content;
+        clear_dice_in_dom();
+        add_dice_to_dom(template_dice_6, rolls, 150);
     };
 });
